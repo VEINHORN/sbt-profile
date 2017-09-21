2722b66be4be70ed7fc817ca07caa35383cb6f1a
VEINHORN <b.korogvich@gmail.com>
2017-09-21 19:25:45 +0300

    implemented several profile tasks

--- build.sbt
+++ build.sbt
@@ -1,14 +1,19 @@
+import sbt.ScriptedPlugin.autoImport.scriptedBufferLog
+
 lazy val root = (project in file("."))
   .settings(
     name := "sbt-profile",
     version := "0.1-SNAPSHOT",
     organization := "com.veinhorn.sbt.plugin",
     scalaVersion := "2.12.3",
-    sbtPlugin := true
+    sbtPlugin := true,
+    libraryDependencies ++= Seq(
+      "org.scalactic" %% "scalactic" % "3.0.1",
+      "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+    ),
+    // ScriptedPlugin.scriptedSettings
+    scriptedLaunchOpts := { scriptedLaunchOpts.value ++
+      Seq("-Xmx1024M", "-XX:MaxPermSize=256M", "-Dplugin.version=" + version.value)
+    },
+    scriptedBufferLog := false
   )
-
-// ScriptedPlugin.scriptedSettings
-scriptedLaunchOpts := { scriptedLaunchOpts.value ++
-  Seq("-Xmx1024M", "-XX:MaxPermSize=256M", "-Dplugin.version=" + version.value)
-}
-scriptedBufferLog := false
--- src/main/scala/com/veinhorn/sbt/plugin/profile/ProfilePlugin.scala
+++ src/main/scala/com/veinhorn/sbt/plugin/profile/ProfilePlugin.scala
@@ -1,15 +1,17 @@
 package com.veinhorn.sbt.plugin.profile
 
+import java.io.PrintWriter
+
 import sbt.Keys._
 import sbt._
 import sbt.complete.Parsers.spaceDelimited
+
+import scala.io.Source
 
 /**
   * Created by Boris Korogvich on 12.09.2017.
   */
 object ProfilePlugin extends AutoPlugin {
-  // override def trigger = allRequirements
-
   type Property = Tuple2[String, String]
 
   case class Profile(id: String,
@@ -20,17 +22,19 @@ object ProfilePlugin extends AutoPlugin {
     // Settings
     val profiles = settingKey[Seq[Profile]]("Specify profiles")
     // Tasks
-    val showProfiles = taskKey[Unit]("Show all profiles")
-    val showProfileProperties = inputKey[Unit]("Show profile properties")
+    lazy val showProfiles = taskKey[Unit]("Show all profiles")
+    lazy val showProfileProperties = inputKey[Unit]("Show profile properties")
+    lazy val applyProfile = taskKey[Unit]("Apply plugin settings")
+
+    lazy val selectProfile = inputKey[Unit]("Select profile")
   }
 
   import autoImport._
 
-  private val init = (state: State) => {
-    /*println("initialization...")
-    println(sourceDirectory)*/
+  /*private val init = (state: State) => {
+    println("initializing...")
     state
-  }
+  }*/
 
   override lazy val projectSettings = Seq(
     showProfiles := {
@@ -41,17 +45,70 @@ object ProfilePlugin extends AutoPlugin {
     },
     /** Should be an ability to print properties for multiple profiles */
     showProfileProperties := {
-      // val args = spaceDelimited("").parsed
-      spaceDelimited("").parsed.foreach { profile =>
-        profiles.value.find(_.id == profile) foreach { p =>
-          println(p.id)
-          p.properties.map { case (key, value) => s"  ")
+      val args = spaceDelimited("").parsed
+
+      args.foreach(println)
+    },
+    /** Apply default profile or passed as parameter */
+    applyProfile := {
+      def replaceInFile(resFile: File) = {
+        println(s"Replacing ${resFile.toPath.toString} ...")
+
+        val res = Source.fromFile(resFile).mkString
+        var replaced = res
+
+        profiles.value.find(_.default).foreach { profile =>
+          println(s"Selected *${profile.id}* profile")
+          profile.properties.foreach { case (key, value) =>
+              val regex = "\\$\\{" + key + "\\}"
+
+              replaced = replaced.replaceAll(regex, value)
+              if (resFile.getName == "creds.properties") {
+                println("regex=" + regex)
+                println("resource=" + res)
+                println("replaced=" + replaced)
+              }
+          }
         }
+        new PrintWriter(resFile) { write(replaced); close() }
       }
 
-      // args.foreach(println)
-    },
-    onLoad in Global ~= (init compose _)
-  )
+      (copyResources in Compile).value
 
+      val resourceFiles = (resources in Compile).value
+      val targetDir = (classDirectory in Compile).value
+
+      /*println("=== Resources")
+      resourceFiles.filter(_.isFile).foreach(f => println(f.toPath.toString))
+
+      println("=== Resources in target")
+      Option(targetDir.listFiles()).map(_.filter(_.isFile)).foreach(_.foreach(f => println(f.toPath.toString)))*/
+
+      Option(targetDir.listFiles())
+        .map(_.filter(_.isFile))
+        .foreach(_.foreach(replaceInFile))
+    },
+    selectProfile := {
+      val args = spaceDelimited("").parsed
+
+      if (args.size == 1) {
+        profiles ~= { profile =>
+          val found = profile.find(_.id == args(0))
+          if (found.isDefined) profile.map(_.copy(default = false))
+              .map { p =>
+                if (p.id == args.head) p.copy(default = true)
+                p
+              }
+          else profile
+          // profile
+          //profile map {
+          //  case p@Profile(id, _, _) if id == args(0) => p.copy()
+          // }
+        }
+      } else {
+        println("Cannot select this profile")
+      }
+      }
+    //,onLoad in Global ~= (init compose _) // some init actions
+  )
 }
